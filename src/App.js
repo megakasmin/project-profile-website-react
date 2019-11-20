8fd2b2f03e46c4caf7c5dd0e6b252bb68b4194cf
megakasmin <meghakasmin99@gmail.com>
2019-11-20 09:44:55 +0700

    Add Router in App component

--- src/App.js
+++ src/App.js
@@ -2,39 +2,41 @@ import React, { Component } from "react";
 import "./App.css";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import Main from "./components/main";
-import { Link } from "react-router-dom";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="demo-big-content">
-        <Layout>
-          <Header
-            className="header-color"
-            title={
-              <Link style={{ textDecoration: "none", color: "white" }} to="/">
-                My Portfolio
-              </Link>
-            }
-            scroll
-          ></Header>
-          <Drawer
-            title={
-              <Link style={{ textDecoration: "none", color: "black" }} to="/">
-                My Portfolio
-              </Link>
-            }
-          >
-            <Navigation>
-              <Link to="/resume">Resume</Link>
-            </Navigation>
-          </Drawer>
-          <Content>
-            <div className="page-content" />
-            <Main />
-          </Content>
-        </Layout>
-      </div>
+      <Router>
+        <div className="demo-big-content">
+          <Layout>
+            <Header
+              className="header-color"
+              title={
+                <Link style={{ textDecoration: "none", color: "white" }} to="/">
+                  My Portfolio
+                </Link>
+              }
+              scroll
+            ></Header>
+            <Drawer
+              title={
+                <Link style={{ textDecoration: "none", color: "black" }} to="/">
+                  My Portfolio
+                </Link>
+              }
+            >
+              <Navigation>
+                <Link to="/resume">Resume</Link>
+              </Navigation>
+            </Drawer>
+            <Content>
+              <div className="page-content" />
+              <Main />
+            </Content>
+          </Layout>
+        </div>
+      </Router>
     );
   }
 }
